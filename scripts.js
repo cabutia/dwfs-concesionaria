b407e18a9f431fe0925c7fc2d4f52a2cc1902aee
Leandro Gomez <leandrogrotnig11@gmail.com>
2019-06-18 20:26:29 -0300

    Resuelto el javascript

--- scripts.js
+++ scripts.js
@@ -1,23 +1,27 @@
 // Elementos a utilizar
 let listaDeAutos = document.getElementById('car-list')
 let formulario = document.getElementById('form-carga')
+let listaDeEstadisticas = document.getElementById('estadisticas')
 
 // Variables globales
-let autos = [
+let autos = []
+let estadisticas = [
     {
-        marca: 'Chevrolet',
-        precio: 35000
-    },
-    {
-        marca: 'Ford',
-        precio: 15000
+        titulo: 'Total de autos',
+        valor () {
+            return autos.length
+        }
     }
 ]
 
-
+// Declaracion de funciones
 function actualizarListaAutos () {
+    if (autos.length === 0) {
+        listaDeAutos.innerHTML = 'Aun no hay autos en la base de datos.'
+        return false
+    }
     let autosHtml = []
-    autos.forEach(auto => {
+    autos.forEach(function (auto) {
         autosHtml.push(`
             <div class="car">
                 <h3>${auto.marca}</h3>
@@ -28,4 +32,46 @@ function actualizarListaAutos () {
             </div>
         `)
     })
+    listaDeAutos.innerHTML = autosHtml.join("\n")
 }
+
+function actualizarEstadisticas () {
+    let estadisticasHtml = []
+    estadisticas.forEach(function (estadistica) {
+        estadisticasHtml.push(`
+            <tr>
+                <td>${estadistica.titulo}</td>
+                <td>${estadistica.valor()}</td>
+            </tr>
+        `)
+    })
+    listaDeEstadisticas.innerHTML = estadisticasHtml.join("\n")
+}
+
+function agregarAuto (marca, precio) {
+    if (!marca || !precio)
+        return alert('Marca o precio invalidos.')
+    autos.push({
+        marca: marca,
+        precio: precio
+    })
+}
+
+function limpiarFormulario () {
+    formulario.precio.value = null
+    formulario.marca.value = null
+    formulario.marca.focus()
+}
+
+function enviarFormulario (evento) {
+    evento.preventDefault()
+    agregarAuto(formulario.marca.value, formulario.precio.value)
+    actualizarListaAutos()
+    actualizarEstadisticas()
+    limpiarFormulario()
+}
+
+// Procedimiento
+actualizarListaAutos()
+actualizarEstadisticas()
+formulario.addEventListener('submit', enviarFormulario)
